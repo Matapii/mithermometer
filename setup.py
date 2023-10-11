e77e20305f445e95718f61d63db35e65284307c7
Matapii <46297405+Matapii@users.noreply.github.com>
2023-10-11 21:28:41 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name='mithermometer',
-    version='0.1.4',
+    version='0.1.5',
     description='Library to read data from Xiaomi Aqara thermometer',
     long_description=readme(),
     long_description_content_type='text/markdown',
